4a86049c8d6e5b3adc75241ba630036c911556fb
Clemens <clemens@gmail.com>
2019-09-16 16:49:33 +0200

    added more possible instrument sources

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 
 base_dir = '/gpfs/exfel/exp/HED/201922/p002550/raw'
 instrument_source = 'HED_IA1_EPIX10K-1/DET/RECEIVER:daqOutput'
+#instrument_source = 'HED_IA1_EPX100-1/DET/RECEIVER:daqOutput'
+#instrument_source = 'HED_IA1_JNGFR-1/DET/MODULE:daqOutput'
 instrument_key = 'data.image.pixels'
 
 @app.route('/run_<run_index>/frame_<frame_index>')
