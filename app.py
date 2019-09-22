77f64dcedda072495b46b260628792fcb6f5847a
Clemens <clemens@gmail.com>
2019-09-22 17:49:49 +0200

    Frames are now got only from trainids who actually have data for the detector

--- app.py
+++ app.py
@@ -17,11 +17,9 @@ import time
 
 @app.route('/run_<run_index>/frame_<frame_index>')
 def get_frame(run_index, frame_index):
-    t1 = time.time()
     data = get_frame_from_karabo(int(run_index), int(frame_index))
     bytestream = io.BytesIO()
     np.save(bytestream, data)
-    print(time.time()-t1)
     return bytestream.getvalue()
 
 
@@ -33,17 +31,23 @@ def get_run(run_index):
     return bytestream.getvalue()
 
 
+def get_collected_ids(run):
+    counts = run.get_data_counts(instrument_source, instrument_key)
+    return counts.index[np.where(counts.array==1)]
+
+
 def get_frame_from_karabo(run_index, frame_index):
     run = RunDirectory(os.path.join(base_dir, 'r{:04d}'.format(run_index)))
-    tid, data = run.train_from_index(frame_index)
+    ids = get_collected_ids(run)
+    tid, data = run.train_from_id(ids[frame_index])
     return data[instrument_source][instrument_key]
 
 
 def get_run_from_karabo(run_index):
     run = RunDirectory(os.path.join(base_dir, 'r{:04d}'.format(run_index)))
-    print(len(run.train_ids))
     data = run.get_array(instrument_source, instrument_key)
     return data
+
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
